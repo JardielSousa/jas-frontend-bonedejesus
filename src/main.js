d30f53d952702bd6c2a86096fe26bafabef8dfe3
Jardiel Sousa <jardiel.sousa@gmail.com>
2021-02-23 20:02:51 -0300

    new docs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import jQuery from "jquery/dist/jquery.min.js";
+import 'waypoints/lib/jquery.waypoints.js'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'waypoints/lib/jquery.waypoints'
 
-window.jQuery = jQuery;
-var $ = window.jQuery;
+global.jQuery = jQuery;
+var $ = global.jQuery;
 window.$ = $;
+// global.Waypoint = Waypoint
 
 Vue.config.productionTip = false
 
